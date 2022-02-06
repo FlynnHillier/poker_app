4ea216ad753b8a4a7561d9e43889e0948227b3f1
FlynnHillier <flynnhillier@gmail.com>
2022-02-06 18:10:49 +0000

    added usericon.js in api_auth

---
+++ backend/api/api_authorised/routes/userIcon.js
@@ -0,0 +1,26 @@
+const path = require("path")
+const express = require("express")
+
+
+//NOT IN USE
+
+
+module.exports = function(mongoClient,config){
+
+
+let Router = express.Router()
+
+
+//** LOGIN **
+Router
+    .route("/")
+    .get((req,res)=>{
+
+        res.status(200).send(req.session.userID)
+        
+        
+    })
+
+
+//KEEP BOTTOM
+return Router}
