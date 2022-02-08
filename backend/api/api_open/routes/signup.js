3564acf1d662f5275c57cb323954ac92ee96fc74
FlynnHillier <flynnhillier@gmail.com>
2022-02-08 20:09:06 +0000

    api 'login.js' mongoose implementation

--- backend/api/api_open/routes/signup.js
+++ backend/api/api_open/routes/signup.js
@@ -15,35 +15,37 @@ function signup(mongoClient,config){
 
 
 
-            const username = req.body.signup_username
-            const password = req.body.signup_password
+        const username = req.body.signup_username
+        const password = req.body.signup_password
 
 
-            mongoClient.connect().then((connection_result)=>{
-                const collection = mongoClient.db("users").collection("standard_users")
+            
 
 
-                collection.findOne({username:username}).then((username_found_result) => {
-                    if(username_found_result){
-                        res.status(403).json({err:"User creation error, user already exists",message:"That username is unavailable"})
-                    } else{
+            config.mongoose_models.user.findOne({username:username}).then((username_found_result) => {
+                
+                if(username_found_result){
+                    res.status(403).json({err:"User creation error, user already exists",message:"That username is unavailable"})
+                } else{
 
-                        collection.insertOne({username:username,password:password}).then((user_creation_result)=>{
+                    new config.mongoose_models.user({
+                        username:username,
+                        password:password
+                    }).save()
 
-                            res.status(200).send({next_url:"/signup/success"})
+                    .then((user_creation_result)=>{
 
-                        }).catch((err)=>{
-                            res.send(503).json({err:err,"message":"Error creating user within database. Database error"})
-                        })
+                        res.status(200).send({next_url:"/signup/success"})
 
-
-                    }
-                })
+                    }).catch((err)=>{
+                        res.send(503).json({err:err,"message":"Error creating user within database. Database error"})
+                    })
 
 
-            }).catch((err)=>{
-                res.status(503).json({message:"Error establishing connection to database.",err:err})
+                }
             })
+
+
         })
 
 
